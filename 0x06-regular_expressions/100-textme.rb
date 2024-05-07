3647b6dfc29b96a17f1b0d100f39e5152011de72
teresiawairimu <teresiawairimu06@gmail.com>
2024-05-07 14:54:35 -0700

    task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\+?\d{11}\b/).join
+
+input = ARGV[0]
+
+sender_regex = input.match(/\[from:([^\]]+)\]/)
+sender = sender_regex ? sender_regex[1] : nil
+
+receiver_regex = input.match(/\[to:([^\]]+)\]/)
+receiver = receiver_regex ? receiver_regex[1] : nil
+
+flags_regex = input.match(/\[flags:([^\]]+)\]/)
+flags = flags_regex ? flags_regex[1] : nil
+
+sender = sender[/\+?\d{11}\b/] || sender
+receiver = receiver[/\+?\d{11}\b/] || receiver
+
+puts "#{sender},#{receiver},#{flags}"
+
