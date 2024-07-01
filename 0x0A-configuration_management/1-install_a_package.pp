4f1342308f473f0172611848f47f0adb4dcf1df5
teresiawairimu <teresiawairimu06@gmail.com>
2024-07-01 12:26:48 -0700

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Install flask from pip3
 exec {'pip3 install flask':
 require => Exec['python-installed'],
-command => '/usr/bin/pip3 install flask==2.1.0'
+command => '/usr/bin/pip3 install flask==2.1.0 werkzeug==0.16.1'
 }
 exec {'python-installed':
 command => '/usr/bin/which python3'
