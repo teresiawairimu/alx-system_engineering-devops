6a2ca01259602f374e5b1f36bb97c6a023b730c5
teresia wairimu <teresiawairimu06@gmail.com>
2024-07-22 09:11:29 -0700

    task 5

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+class { 'nginx':
+  package_ensure => 'present',
+  service_ensure => 'running',
+  service_enable => true,
+}
+file { 'etc/nginx/sites-available/default':
+  ensure => 'file',
+  content => template('nginx/default.erb'),
+  notify => service['nginx'],
+}
+file { 'var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+file { '/var/www/html/404.html':
+  ensure  => 'file',
+  content => "Ceci n'est pas une page",
+}
+service { 'nginx':
+  ensure     => 'running',
+  enable     => true,
+  require    => File['/etc/nginx/sites-available/default'],
+}
+nginx::resource::vhost { 'default':
+  ensure    => present,
+  www_root  => '/var/www/html',
+  listen_port => 80,
+  location_cfg_append => {
+    '/' => {
+      'try_files' => '$uri $uri/ =404',
+    },
+    '/redirect_me' => {
+      'return' => '301 https://www.example.com',
+    },
+    '~* \.html$' => {
+      'error_page' => '404 /404.html',
+    },
+  },
+}
