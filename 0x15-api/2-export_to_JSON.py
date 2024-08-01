6b146b4916933905af04ca1f9957b0b0ffff10ec
teresiawairimu <teresiawairimu06@gmail.com>
2024-07-31 18:21:21 -0700

    task 1

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Script uses REST API
 To derive the information about employee's TODO list progress
-The information should be exported in csv format
+The information should be exported in JSON format
 """
-import csv
+import json
 import requests
 import sys
 
@@ -13,10 +13,10 @@ def get_employee_todo_progress(employee_id):
     Parameters:
     int: The employee Id
     Returns:
-    int: the employee_id
-    str: employee's name
+    int: the user_id
     str: task completed status
     str: the task's title
+    str: username
     """
     employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
     employee_response = requests.get(employee_url)
@@ -24,7 +24,7 @@ def get_employee_todo_progress(employee_id):
         print("User not found")
         return
 
-    user_data = employee_response.json()
+    user_data = user_response.json()
     user_name = user_data.get("username")
     user_id = user_data.get("id")
 
@@ -37,18 +37,20 @@ def get_employee_todo_progress(employee_id):
         return
 
     todos_data = todos_response.json()
+    json_data = {
+        str(user_id): [
+            {
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user_username
+            }
+            for task in todos_data
+        ]
+    }
+    json_filename = f"{user_id}.json"
+    with open(json_filename, mode='w', encoding='utf-8') as json_file:
+        json.dump(json_data, json_file)
 
-    csv_filename = f"{user_id}.csv"
-    with open(
-            csv_filename, mode='w', newline='', encoding='utf-8'
-            ) as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for task in todos_data:
-            csv_writer.writerow(
-                    [user_id, user_name, str(
-                        task.get("completed")
-                        ), task.get("title")]
-                    )
 
 
 if __name__ == "__main__":
