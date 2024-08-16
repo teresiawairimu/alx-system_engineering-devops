42658cb82c91218ba2052b9a8721c97b43c2efd2
teresiawairimu <teresiawairimu06@gmail.com>
2024-08-16 07:35:40 -0700

    task 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Fixes a typo in wp-settings.php
 exec { 'fix-wordpress-typo':
-  command => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' /var/www/html/wp-settings.php",
-  onlyif  => "/bin/grep -q 'class-wp-locale.phpp' /var/www/html/wp-settings.php",
-  require => File['/var/www/html/wp-settings.php'],
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
 
