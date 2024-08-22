3305f3d6df5c1beb54ad135b17057ebdbf742110
teresiawairimu <teresiawairimu06@gmail.com>
2024-08-22 21:01:00 +0000

    task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# Fix Nginx to handle more file requests
+
+exec { 'Fixing the file descriptor limit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
